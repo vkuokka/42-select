e1f8d3769f8d7ff10813571d33d86481db47c7bb
Vesa Kuokka <vesakuokka@hotmail.com>
2020-03-16 14:50:25 +0200

    Fixed issue with printing of the selected elements.

--- srcs/listen_keys.c
+++ srcs/listen_keys.c
@@ -14,15 +14,20 @@
 
 static void	print_selected(t_terminal *term)
 {
+	int	printed;
 	size_t	i;
 
+	printed = 0;
 	i = -1;
 	while (++i < term->length)
 		if (term->select[i])
 		{
 			write(1, term->args[i], ft_strlen(term->args[i]));
 			write(1, " ", 1);
+			printed = 1;
 		}
+	if (printed)
+		write(1, "\n", 1);
 }
 
 static void	check_arrows(int sum, t_terminal *term)
@@ -40,8 +45,6 @@ static void	check_other(int sum, t_terminal *term)
 	if (sum == ESC)
 	{
 		config_terminal(1, term);
-		tputs(tgetstr("cl", NULL), 1, print_char);
-		tputs(tgetstr("cd", NULL), 1, print_char);
 		free(term->select);
 		free(term);
 		exit(0);
@@ -54,8 +57,11 @@ static void	check_other(int sum, t_terminal *term)
 	}
 	else if (sum == ENTER)
 	{
+		config_terminal(1, term);
 		print_selected(term);
-		check_other(ESC, term);
+		free(term->select);
+		free(term);
+		exit(0);
 	}
 	else if (sum == BACK || sum == DEL)
 		if (delete_element(term))
